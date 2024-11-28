f392bda31d49588c97503f09ef990319b0d87358
nomass1 <watcharawit9669@gmail.com>
2024-11-29 01:51:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // กำหนดคะแนนใน JavaScript
-const studentScore = 1; // เปลี่ยนคะแนนได้ที่นี่
+const studentScore = 100; // เปลี่ยนคะแนนได้ที่นี่
 
 // แสดงคะแนนในหน้าเว็บ
 document.getElementById('score-display').textContent = studentScore;
